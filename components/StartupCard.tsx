9873e8a24c40d798bb3c6066c284b6db00ca32b5
SamChukwuemeka <engrsamchukwuemeka@gmail.com>
2025-08-20 01:05:43 +0100

    updates

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -12,6 +12,7 @@ import Image from "next/image";
 import CloudinaryImage from "@/components/CloudinaryImage";
 import {Button} from "@/components/ui/button";
 
+
 const StartupCard = ( {post}:{post: any})  => {
     const { _createdAt, views, title, description, category } = post;
     return (
